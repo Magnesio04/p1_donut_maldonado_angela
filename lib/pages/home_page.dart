2692f5a4ad23bc46932c02f3d3dc0fb05571de9a
Angie Magnesio <angiemagnesio@gmail.com>
2024-10-14 12:53:05 -0600

    Rockytec TabBar añadido

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../utils/my_tab.dart';
 
+import '../tab/burger_tab.dart';
+import '../tab/donut_tab.dart';
+import '../tab/pancake_tab.dart';
+import '../tab/pizz_tab.dart';
+import '../tab/smoothie_tab.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -68,6 +73,15 @@ class _HomePageState extends State<HomePage> {
         //Tab bar
         TabBar(tabs: myTabs),
         //Tab bar view
+        const Expanded(
+          child: TabBarView(children: [
+          DonutTab(),
+          BurgerTab(),
+          SmoothieTab(),
+          PanCakeTab(),
+          PizzTab()
+        ])
+        )
         //
         //Total del carrito      
       ],),
---
+++ lib/tab/pizz_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PizzTab extends StatelessWidget {
+  const PizzTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("PizzaTab"));
+  }
+}
