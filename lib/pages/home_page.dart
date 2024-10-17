f2fb187e42c233d51b2e2e225631503bbf37afea
Angie Magnesio <angiemagnesio@gmail.com>
2024-10-17 10:55:21 -0600

    DonutTile.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-
 import '../utils/my_tab.dart';
+
+import '../tab/donut_tab.dart';
+import '../tab/burger_tab.dart';
+import '../tab/pancakes_tab.dart';
+import '../tab/pizza_tab.dart';
+import '../tab/smoothie_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,67 +15,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  List<Widget> myTabs = [
-    //Donut tab
-    const MyTab(
-      iconPath: "lib/icons/donut.png",
-      ),
-    //burger tab
-    const MyTab(
-      iconPath: "lib/icons/burger.png",
-      ),
-    //smoothie tab
-    const MyTab(
-      iconPath: "lib/icons/smoothie.png",
-      ),
-    //pancake tab
-    const MyTab(
-      iconPath: "lib/icons/pancakes.png",
-      ),
+List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //burguer tab
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    //smothie tab
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
     //pizza tab
-    const MyTab(iconPath: "lib/icons/pizza.png",)
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
   ];
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
+    return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
         appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Icon(
-            Icons.menu, 
-            color:Colors.grey[800],
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
             ),
-        ),
-        actions: const [Padding(
-          padding: EdgeInsets.only(right: 24.0),
-          child: Icon(Icons.person),
-        )],
-        ),
-        body:  Column(
-      children: [
-        // Texto "I want to eat"
-        const Padding(
-          padding: EdgeInsets.all(24.0),
-          child: Row(
-            children: [
-              Text("Friday ", style: TextStyle(fontSize: 24,),),
-              Text("party!!", style: TextStyle(fontSize: 24, 
-              fontWeight: FontWeight.bold, 
-              decoration: TextDecoration.underline)
-              ), 
-            ],
           ),
-        ), 
-        //Tab bar
-        TabBar(tabs: myTabs),
-        //Tab bar view
-        //
-        //Total del carrito      
-      ],),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto: I want to eat
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text("Im Pablo and  ", style: TextStyle(fontSize: 24)),
+                  Text("i cook:",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline)),
+                ],
+              ),
+            ),
+            //Tab bar
+            TabBar(tabs: myTabs),
+            //Tab bar view
+            Expanded(
+              child: TabBarView(children: [
+              DonutTab(),
+              const BurgerTab(),
+              const SmoothieTab(),
+              const PanCakeTab(),
+              const PizzaTab()
+            ],
+            ),
+            //Total del carrito
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/utils/my_tab.dart
+++ lib/utils/my_tab.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 
 class MyTab extends StatelessWidget {
   final String iconPath;
@@ -8,8 +8,16 @@ class MyTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Tab(
       child: Container(
-        color: const Color.fromARGB(255, 201, 94, 234),
-        child: Image.asset(iconPath),),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Image.asset(
+          iconPath,
+          color:Colors.grey[600],
+        )
+      ),
     );
   }
 }
